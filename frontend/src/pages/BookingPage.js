1a7ed8f759d67ace9e4c7bd8f305c85da1f0b38f
yakshithakamma258-collab <yakshithakamma477@gmail.com>
2026-03-19 12:28:33 +0530

    Update BookingPage.js

--- frontend/src/pages/BookingPage.js
+++ frontend/src/pages/BookingPage.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "../style.css";
 
-function Bookingpage() {
+function BookingPage() {
 
   const [bookings, setBookings] = useState([]);
 
@@ -63,4 +63,4 @@ function Bookingpage() {
   );
 }
 
-export default Bookingpage;
+export default BookingPage;
